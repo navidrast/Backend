2a6faaac6b682051456ab1cb05a23212d65d1ac3
linivek <linivek1986@gmail.com>
2024-11-17 16:44:44 +1100

    更新宠物模型：移除不必要的验证器导入

--- pets/models.py
+++ pets/models.py
@@ -3,7 +3,6 @@
 from django.db import models
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
-from django.core.validators import MinValueValidator, MaxValueValidator
 from services.models import DogSize
 import uuid
 
